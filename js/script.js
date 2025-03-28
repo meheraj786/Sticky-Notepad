75968157d2eb5d92172ba6d02c459f9e20d4234a
Mehraj Hosen <meherajhosen786@gmail.com>
2025-03-29 04:51:28 +0600

    bug fix

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
-const saveBtn= document.querySelector(".saveBtn")
-const delBtn= document.querySelector(".delBtn")
-const editBtn= document.querySelector(".editBtn")
-const addBtn= document.querySelector("#main .add")
+const addBtn = document.querySelector("#main .add");
+const logo = document.querySelector(".logo");
 let colors = [
   "var(--Yellow)",
   "var(--Green)",
@@ -15,41 +13,51 @@ let colors = [
   "var(--Coral)",
   "var(--Lilac)"
 ];
-let index= Math.floor(Math.random()*colors.length)
-  let color= colors[index]
-  addBtn.style.backgroundColor=color
-  let input= document.createElement("p")
-  input.innerHTML=`Write here and save it <button onclick="save(this)" class="saveBtn"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" fill-rule="evenodd"><path d="m12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.018-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"/><path fill="currentColor" d="M6 2a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6.414A2 2 0 0 0 19.414 5L17 2.586A2 2 0 0 0 15.586 2zm0 2h9.586L18 6.414V20H6zm10.238 6.793a1 1 0 1 0-1.414-1.414l-4.242 4.243l-1.415-1.415a1 1 0 0 0-1.414 1.414l2.05 2.051a1.1 1.1 0 0 0 1.556 0l4.88-4.879Z"/></g></svg></button>
-  <button onclick="edit(this)" class="editBtn"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M7 7H6a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2-2v-1"/><path d="M20.385 6.585a2.1 2.1 0 0 0-2.97-2.97L9 12v3h3zM16 5l3 3"/></g></svg></button>
-  <button onclick="del(this)" class="delBtn"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V9c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2zM18 4h-2.5l-.71-.71c-.18-.18-.44-.29-.7-.29H9.91c-.26 0-.52.11-.7.29L8.5 4H6c-.55 0-1 .45-1 1s.45 1 1 1h12c.55 0 1-.45 1-1s-.45-1-1-1"/></svg></button>`
-  document.querySelector(".notes").appendChild(input)
-  input.contentEditable=true
-  input.style.backgroundColor=color
 
 function add() {
-  let index= Math.floor(Math.random()*colors.length)
-  let color= colors[index]
-  let input= document.createElement("p")
-  input.innerHTML=`Write here and save it <button onclick="save(this)" class="saveBtn"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" fill-rule="evenodd"><path d="m12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.018-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"/><path fill="currentColor" d="M6 2a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6.414A2 2 0 0 0 19.414 5L17 2.586A2 2 0 0 0 15.586 2zm0 2h9.586L18 6.414V20H6zm10.238 6.793a1 1 0 1 0-1.414-1.414l-4.242 4.243l-1.415-1.415a1 1 0 0 0-1.414 1.414l2.05 2.051a1.1 1.1 0 0 0 1.556 0l4.88-4.879Z"/></g></svg></button>
-  <button onclick="edit(this)" class="editBtn"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M7 7H6a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2-2v-1"/><path d="M20.385 6.585a2.1 2.1 0 0 0-2.97-2.97L9 12v3h3zM16 5l3 3"/></g></svg></button>
-  <button onclick="del(this)" class="delBtn"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V9c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2zM18 4h-2.5l-.71-.71c-.18-.18-.44-.29-.7-.29H9.91c-.26 0-.52.11-.7.29L8.5 4H6c-.55 0-1 .45-1 1s.45 1 1 1h12c.55 0 1-.45 1-1s-.45-1-1-1"/></svg></button>`
-  document.querySelector(".notes").appendChild(input)
-  input.contentEditable=true
-  input.style.backgroundColor=color
-  addBtn.style.backgroundColor=color
-}
-function save(elem) {
-  elem.parentElement.contentEditable = false;
-  elem.style.display = "none"; // Save button hide
-  elem.nextElementSibling.style.display = "inline-block"; // Edit button show
+  let index = Math.floor(Math.random() * colors.length);
+  let color = colors[index];
+
+  // Create note container
+  let noteContainer = document.createElement("div");
+  noteContainer.classList.add("note-container");
+
+  // Create textarea
+  let textarea = document.createElement("textarea");
+  textarea.placeholder = "Write your note here...";
+  textarea.style.backgroundColor = color;
+  textarea.style.width = "100%";
+  textarea.style.minHeight = "250px";
+  textarea.style.padding = "20px";
+  textarea.style.fontSize = "22px";
+  textarea.style.borderRadius = "10px";
+  textarea.style.fontFamily = '"Patrick Hand", cursive';
+  textarea.style.letterSpacing = "2px";
+  textarea.style.border = "1px solid white";
+  textarea.style.textAlign = "left";
+  textarea.style.verticalAlign = "top";
+  textarea.style.resize = "none"; // Prevent resizing
+
+  // Create delete button
+  let delBtn = document.createElement("button");
+  delBtn.classList.add("delBtn");
+  delBtn.innerHTML = ` <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V9c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2zM18 4h-2.5l-.71-.71c-.18-.18-.44-.29-.7-.29H9.91c-.26 0-.52.11-.7.29L8.5 4H6c-.55 0-1 .45-1 1s.45 1 1 1h12c.55 0 1-.45 1-1s-.45-1-1-1"/></svg>`;
+  delBtn.onclick = function() {
+    noteContainer.remove();
+  };
+
+  // Append textarea and delete button to note container
+  noteContainer.appendChild(textarea);
+  noteContainer.appendChild(delBtn);
+
+  // Append note container to notes section
+  document.querySelector(".notes").prepend(noteContainer);
+
+  // Update add button and logo color
+  addBtn.style.backgroundColor = color;
+  logo.style.color = color;
 }
 
-function edit(elem) {
-  elem.parentElement.contentEditable = true;
-  elem.style.display = "none"; // Edit button hide
-  elem.previousElementSibling.style.display = "inline-block"; // Save button show
+function del(elem) {
+  elem.parentElement.remove();
 }
-
-function del(elem){
-elem.parentElement.remove()
-  }
